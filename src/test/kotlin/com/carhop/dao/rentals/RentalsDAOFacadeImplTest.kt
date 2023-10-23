ec77e51fb3e63eddbd19c0eb2fb1d8856a46afa5
Janvanderwaal <janvanderwaal@outlook.com>
2023-10-23 18:33:33 +0200

    Unit tests Rentals aangevuld, unit tests Users afgerond

--- src/test/kotlin/com/carhop/dao/rentals/RentalsDAOFacadeImplTest.kt
+++ src/test/kotlin/com/carhop/dao/rentals/RentalsDAOFacadeImplTest.kt
@@ -3,20 +3,18 @@ package com.carhop.dao.rentals
 import com.carhop.models.Rental
 import io.ktor.server.testing.*
 import kotlinx.datetime.*
-import org.junit.Before
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 
 class RentalsDAOFacadeImplTest {
 
-    private lateinit var rentalsDAO: RentalsDAOFacade
-    @Before
-    fun setUp() {
-        rentalsDAO = RentalsDAOFacadeImpl()
-    }
     @Test
-    suspend fun checkIfRentalIsInPast1() = testApplication {
+
+    fun checkIfRentalIsInPast1() = testApplication {
+            lateinit var rentalsDAO: RentalsDAOFacade
+            rentalsDAO = RentalsDAOFacadeImpl()
 
             val currentDate = Clock.System.todayIn(TimeZone.currentSystemDefault())
             val futureStartDate = currentDate.plus(1, DateTimeUnit.DAY)
@@ -31,7 +29,53 @@ class RentalsDAOFacadeImplTest {
             )
 
             val result = rentalsDAO.checkIfRentalIsInPast(rental)
+        assertTrue(result)
 
+
+    }
+    @Test
+
+    fun checkIfRentalIsInPast2() = testApplication {
+        lateinit var rentalsDAO: RentalsDAOFacade
+        rentalsDAO = RentalsDAOFacadeImpl()
+
+        val currentDate = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val futureStartDate = currentDate.minus(2, DateTimeUnit.DAY)
+        val futureEndDate = currentDate.minus(1, DateTimeUnit.DAY)
+        val rental = Rental(
+            id = 1,
+            carId = 1,
+            renterId = 1,
+            startDate = futureStartDate,
+            endDate = futureEndDate,
+            cost = 100.0
+        )
+
+        val result = rentalsDAO.checkIfRentalIsInPast(rental)
+        assertFalse(result)
+
+
+    }
+    @Test
+
+    fun checkIfRentalIsInPast3() = testApplication {
+        lateinit var rentalsDAO: RentalsDAOFacade
+        rentalsDAO = RentalsDAOFacadeImpl()
+
+        val currentDate = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val futureStartDate = currentDate
+        val futureEndDate = currentDate
+        val rental = Rental(
+            id = 1,
+            carId = 1,
+            renterId = 1,
+            startDate = futureStartDate,
+            endDate = futureEndDate,
+            cost = 100.0
+        )
+
+        val result = rentalsDAO.checkIfRentalIsInPast(rental)
+        System.out.println(result)
         assertFalse(result)
 
 
--- src/test/kotlin/com/carhop/dao/users/UsersDAOFacadeImplTest.kt
+++ src/test/kotlin/com/carhop/dao/users/UsersDAOFacadeImplTest.kt
@@ -1,98 +1,138 @@
 package com.carhop.dao.users
 
 
+import com.carhop.dto.users.LoginRequestDTO
 import com.carhop.dto.users.RegisterUserDto
 import com.carhop.models.User
-import com.carhop.module
 import com.carhop.plugins.DatabaseFactory
+import com.carhop.plugins.configureRouting
+import com.carhop.plugins.configureSecurity
+import com.carhop.utils.TokenManager
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
-import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
 
 
 class UsersDAOFacadeImplTest {
 
     @Test
-    fun registerUser() = runBlocking {
-
-        val server = embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-
-        try {
-            System.out.println("stap1")
-            server.start(wait = false)
-            System.out.println("stap2")
-            DatabaseFactory.init()
-            System.out.println("stap3")
-            val newUser = RegisterUserDto(
-                "Jero",
-                "de Leeuw",
-                "Jero@test.com",
-                "123456",
-                0,
-                "user"
-            )
-            System.out.println(newUser)
-            val registeredUser = userDAO.registerUser(newUser)
-            System.out.println(registeredUser)
-            val expectedUser = registeredUser?.let {
-                User(
-                    id = it.id,
-                    firstName = newUser.firstName, // Fix here
-                    lastName = newUser.lastName,
-                    email = newUser.email,
-                    password = newUser.password,
-                    drivingScore = newUser.drivingScore,
-                    userType = newUser.userType
-                )
-            }
-            System.out.println(expectedUser)
-
-            assertEquals(expectedUser, registeredUser)
-        } catch (e: Exception) {
-            fail("Exception occurred during test: ${e.message}")
-        } finally {
-            server.stop(0L, 0L)
+    fun registerUser() = testApplication {
+        application {
+            configureSecurity()
+            configureRouting()
         }
+        val client = createClient {
+            this@testApplication.install(ContentNegotiation) {
+                json(Json { prettyPrint = true
+                    isLenient = true})
+            }
+        }
+        DatabaseFactory.init()
+
+        val userRegisterReqeust = RegisterUserDto("testUser", "testLastName", "test@email.com", "test", 0,"user")
+        val json = Json.encodeToString(userRegisterReqeust)
+
+        val response = client.post("/users/register") {
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            setBody(json)
+        }
+        val statusCode = response.status
+        val responseBody = response.body<String>()
+
+        assertEquals(HttpStatusCode.OK, statusCode)
+
+        lateinit var userDAO: UsersDAOFacade
+        userDAO = UsersDAOFacadeImpl()
+        val registeredUser = LoginRequestDTO(userRegisterReqeust.email,userRegisterReqeust.password )
+        val loginRegisteredUser = userDAO.loginUser(registeredUser)
+
+        if (loginRegisteredUser != null) {
+            assertEquals(userRegisterReqeust.email, loginRegisteredUser.email)
+        }
+        val deleteRegisteredUser = loginRegisteredUser?.let { userDAO.deleteUser(it.id) }
+
     }
 
     @Test
     fun loginUser() = testApplication {
-
+        application {
+            configureSecurity()
+            configureRouting()
+        }
         val client = createClient {
             this@testApplication.install(ContentNegotiation) {
                 json(Json { prettyPrint = true
-                isLenient = true})
+                    isLenient = true})
             }
         }
-        System.out.println("client:")
-        System.out.println(client)
-        val response = client.post("users/register") {
-            contentType(ContentType.Application.Json)
-            setBody(RegisterUserDto("Jan", "de Leeuw", "Jeroen@tst.com", "123456", 0,"user"))
+        DatabaseFactory.init()
+
+        val loginRequest = LoginRequestDTO("Jeroen@test.com", "123456")
+        val json = Json.encodeToString(loginRequest)
+
+        val response = client.post("/users/login") {
+            header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            setBody(json)
         }
 
-        assertEquals(HttpStatusCode.OK, response.status)
+        val statusCode = response.status
+        val responseBody = response.body<String>()
+
+        assertEquals(HttpStatusCode.OK, statusCode)
+
+        System.out.println("response:")
+        System.out.println(responseBody)
     }
 
     @Test
-    fun getUser() {
+    fun getUser() = testApplication {
+        application {
+            configureSecurity()
+            configureRouting()
+        }
+        val client = createClient {
+            this@testApplication.install(ContentNegotiation) {
+                json(Json { prettyPrint = true
+                    isLenient = true})
+            }
+        }
+        DatabaseFactory.init()
+
+        lateinit var userDAO: UsersDAOFacade
+        userDAO = UsersDAOFacadeImpl()
+        val Expecteduser = userDAO.getUser(2)
+        val tokenManager = TokenManager()
+            val token = Expecteduser?.let { tokenManager.generateJWTToken(it) }
+
+        val response = client.get("/users/profile/2"){
+            headers { append(HttpHeaders.Authorization, "Bearer " + token) }
+
+        }.body<String>()
+
+
+
+        //val responseBody = response.body<String>()
+        System.out.println("response body:")
+        System.out.println(response)
+        val user = Json.decodeFromString<User>(response)
+        System.out.println("response user:")
+        System.out.println(user)
+
+
+
+
+        assertEquals(Expecteduser, Expecteduser)
+
     }
 
-    @Test
-    fun updateUser() {
-    }
 
-    @Test
-    fun deleteUser() {
-    }
 }
+
+
